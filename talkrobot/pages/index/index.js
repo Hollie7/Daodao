f22b0b156dfd5aae9396ac72ad4c37b5485aaec1
KSCzzZ <37904431+KSCzzZ@users.noreply.github.com>
2021-02-02 13:20:10 +0800

    Add files via upload

--- talkrobot/pages/index/index.js
+++ talkrobot/pages/index/index.js
@@ -41,7 +41,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    text_now:"000",
+    text_now:"",
     tempData:null,
     btn3_judge:app.globalData.index_btn3_judge
   },
@@ -108,24 +108,24 @@ Page({
       })
     }
     var that = this;
-    if(app.globalData.voice_text){
+    /*if(app.globalData.voice_text){*/
       wx.request({
-        url: 'http://60.205.169.246:8080/Test',
+        url: 'http://8.136.120.87:8080/Test',
         method:'POST',
         header: {
           'content-type':'application/x-www-form-urlencoded',
           'Accept': 'application/json'
         },
         data:{
-          /*text:"最近一段时间，大概从 12 月开始的，情绪特别低落，12 月是觉得不开心和沮丧，1 月份以来的最近几天，每天都会哭，" +
+          text:"最近一段时间，大概从 12 月开始的，情绪特别低落，12 月是觉得不开心和沮丧，1 月份以来的最近几天，每天都会哭，" +
         "不知道为什么，莫名就觉得孤单，被抛弃，自己一无是处。以前情绪不好的时候，哭过一次大概就好了，三天前，因为觉得特别孤独，又没人可以说，" +
         "特别难过，哭了一个晚上，但是第二天，电话里，男票的一句很普通的话戳中了我，他说——下学期准备考驾照，反正你也不想考。我瞬间就有被抛弃的感觉了，" +
         "觉得我不想长大，只想躲起来，看着人群来来往往，却没有人可以陪我，和我同路，听我说话，我只能是一个人。可能是大三上学期就要结束了，" +
         "不是考研就是要找工作了，回首了下目前为止的大学，想的好多要做的事，都没有做，大部分精力都用在和惰性斗争了，心里有点恐惧和自责。" +
         "这几天几乎每天都要哭，还是觉得情绪里有无穷无尽的悲伤，低沉地我不知道怎么办了，我性格本来就有点内向，不能尽情开心，压抑着自己的那种，" +
         "平常大多是不高兴也不悲伤的情绪，看过一些心理学的书，怀疑自己可能有神经症，大一暑假去号脉的时候，医生说我有神经官能紊乱，那时候就有点不开心，" +
-        "现在情绪的情况这样，要不要去看医生吃药？真的好难受啊。"*/
-        text:app.globalData.voice_text
+        "现在情绪的情况这样，要不要去看医生吃药？真的好难受啊。"
+        /*text:app.globalData.voice_text*/
       },
   
         success: function (res) {
@@ -143,13 +143,25 @@ Page({
         }
       })
     
-    }
+    /*}*/
   },
 
   ToWordcloud:function(){
     wx.navigateTo({
       url: '/pages/wordcloud/wordcloud'
   })
+  },
+
+  Reset:function(){
+    app.globalData.voice_text = null
+    app.globalData.index_btn3_judge = true
+    app.globalData.judge = false
+    app.globalData.backEndData = {"wordCloud":[{"name":"初始","textSize":30},{"name":"初始","textSize":25},{"name":"初始","textSize":20},{"name":"初始","textSize":20},{"name":"初始","textSize":20},{"name":"初始","textSize":15}],"RadarPlot":{"categories":["初始(8)","初始(8)","初始(5)","初始(5)","初始(3)","初始(3)","初始(2)","初始(2)"],"series":{"name":"情绪特征雷达图","data":["8","8","5","5","3","3","2","2"]}},"EmotionScore":"0"}
+    this.setData({
+      text_now:"",
+      btn3_judge:app.globalData.index_btn3_judge
+    }
+    )
   }
 
 })
